ae8af0a965b517e1d45124a0f0f34271eaefdb47
Chris Posada <chris@cpsharp.net>
2019-12-06 12:36:17 -0500

    solved subarray sum equals

---
+++ Arrays/SubarraySumEqualsK.cs
@@ -0,0 +1,52 @@
+﻿namespace CodeChallenges.Arrays
+{
+    public class SubarraySumEqualsK
+    {
+        public int BFSubarraySum(int[] nums, int k)
+        {
+            int count = 0;
+            for (int start = 0; start < nums.Length; start++)
+            {
+                for (int end = start + 1; end <= nums.Length; end++)
+                {
+                    int sum = 0;
+                    for (int i = start; i < end; i++)
+                    {
+                        sum += nums[i];
+                    }
+
+                    if (sum == k)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int SubarraySum(int[] nums, int k)
+        {
+            int count = 0;
+
+            int[] sum = new int[nums.Length + 1];
+
+            sum[0] = 0;
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                sum[i] = sum[i - 1] + nums[i - 1];
+            }
+
+            for (int start = 0; start < nums.Length; start++)
+            {
+                for (int end = start + 1; end <= nums.Length; end++)
+                {
+                    if (sum[end] - sum[start] == k)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
