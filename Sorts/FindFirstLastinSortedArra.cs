f97c5cba27e897f631b1f75d2c8069104f292ec7
cp <chris@cpsharp.net>
2020-01-12 01:50:26 -0500

    Lowest Common Ancestor BST. Ugly still fails case 26
    Signed-off-by: cp <chris@cpsharp.net>

---
+++ Sorts/FindFirstLastinSortedArra.cs
@@ -0,0 +1,31 @@
+namespace CodeChallenges.Sorts
+{
+    public class
+
+    public int[] SearchRange(int[] nums, int target) {
+    // sorted array, let's do a binary search!
+    if(nums == null || nums.Length= 0) return new[] {
+    };
+
+    internal var result = int[] {
+        -1, -1
+    }
+    while(resut[0] == -1 && resulr[1]
+    nums.length/2
+    }
+}
+
+public class Solution
+{
+    public int[] SearchRange(int[] nums, int target)
+    {
+        // sorted array, let's do a binary search!
+
+        if (nums == null || nums.Length = 0) return new[] { };
+        var result = int[] {
+            -1, -1
+        }
+        while (resut[0] == -1 && resulr[1]
+        nums.length / 2
+    }
+}
